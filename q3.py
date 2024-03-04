8f068e536a5ea18ebf1ac986f0aa1528fa7298dd
yxhong-tw <yxhong.tw@gmail.com>
2024-03-04 17:48:07 +0800

    Update the calculating method of q3.py

--- q3.py
+++ q3.py
@@ -1,11 +1,13 @@
 from typing import List
 
 
-def calculate_cross_product(v1: List[int], v2: List[int]) -> List[int]:
-    return [
-        v1[1] * v2[2] - v1[2] * v2[1], v1[2] * v2[0] - v1[0] * v2[2],
-        v1[0] * v2[1] - v1[1] * v2[0]
-    ]
+def calculate_cross_product(v1: List[List[int]], v2: List[int]) -> List[int]:
+    cross_product = []
+
+    for column in v1:
+        cross_product.append(sum([column[i] * v2[i] for i in range(3)]))
+
+    return cross_product
 
 
 if __name__ == "__main__":
@@ -18,7 +20,7 @@ if __name__ == "__main__":
     #     for element in input("Enter the second vector: ")[1:-1].split("; ")
     # ]
 
-    v1 = [2, 1, 5]
+    v1 = [[0, -5, 1], [5, 0, -2], [-1, 2, 0]]
     v2 = [-6, 4, 8]
 
     cross_product = str(calculate_cross_product(v1, v2)).replace(",", ";")
